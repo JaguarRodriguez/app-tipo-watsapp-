50c989798d3c8fdf7312bd0f723ef0d5a108397c
JaguarRodriguez <sisopsky@gmail.com>
2018-05-30 14:26:49 -0500

    Commit 1

---
+++ METEOR/sisoChat6/src/declarations.d.ts
@@ -0,0 +1,36 @@
+  /* 
+
+
+  Los archivos de declaración son cómo el compilador de Typescript sabe sobre la información de tipo (o forma) de un objeto. 
+  Son lo que hace que intellisense funcione y haga que Typescript sepa todo sobre tu código. 
+  Un módulo comodín se declara a continuación para permitir el uso de bibliotecas de terceros en una aplicación, incluso si no lo hacen 
+  proporcionar sus propias declaraciones de tipo . 
+  Para obtener más información sobre el uso de bibliotecas de terceros en una aplicación Ionic, consulte los documentos aquí: 
+  http: //ionicframework.com/docs/v2/resources/third-party-libs/ 
+  Para obtener más información sobre los archivos de definición de tipo , consulte los documentos de Typescript aquí: 
+  https: //www.typescriptlang.org/docs/handbook/declaration-files/introduction.html 
+
+  
+  */ 
+
+
+
+
+
+
+
+  /*
+  Declaration files are how the Typescript compiler knows about the type information(or shape) of an object.
+  They're what make intellisense work and make Typescript know all about your code.
+  A wildcard module is declared below to allow third party libraries to be used in an app even if they don't
+  provide their own type declarations.
+  To learn more about using third party libraries in an Ionic app, check out the docs here:
+  http://ionicframework.com/docs/v2/resources/third-party-libs/
+  For more info on type definition files, check out the Typescript docs here:
+  https://www.typescriptlang.org/docs/handbook/declaration-files/introduction.html
+*/
+
+
+
+
+
